cf890d4744b49b89b6d80990a2e50f3071f35fe1
Andy Miller <amcoder@gmail.com>
2016-04-27 13:08:29 -0400

    Add issue comment handler

--- routes/github.js
+++ routes/github.js
@@ -2,11 +2,23 @@ var express = require('express');
 var debug = require('debug')('amcoderbot:github');
 var router = express.Router();
 
+var logRequest = function(req, res, next) {
+  debug("Headers: " + JSON.stringify(req.headers, null, 2));
+  debug("Body: " + JSON.stringify(req.body, null, 2));
+  next();
+}
+
+var issueComment = function(req, res, next) {
+  if(req.get("x-github-event") === "issue_comment") {
+    debug("Issue comment");
+  }
+  next();
+}
+
 var masterPush = function(req, res, next) {
   if(req.get("x-github-event") === "push") {
     debug("Master pushed");
   }
-
   next();
 }
 
@@ -15,12 +27,10 @@ var defaultResponse = function(req, res, next) {
 }
 
 /* The github webhook endpoint */
-router.post('/webhook', function(req, res, next) {
-  debug("Headers: " + JSON.stringify(req.headers, null, 2));
-  debug("Body: " + JSON.stringify(req.body, null, 2));
-  next();
-},
-masterPush,
-defaultResponse);
+router.post('/webhook',
+    logRequest,
+    masterPush,
+    issueComment,
+    defaultResponse);
 
 module.exports = router;
