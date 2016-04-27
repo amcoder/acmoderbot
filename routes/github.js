7cc07170ef722932f5d7bbfa844640947078fe9e
Andy Miller <amcoder@gmail.com>
2016-04-27 13:02:20 -0400

    Add push listener

--- routes/github.js
+++ routes/github.js
@@ -2,11 +2,22 @@ var express = require('express');
 var debug = require('debug')('amcoderbot:github');
 var router = express.Router();
 
+var masterPush = function(req, res, next) {
+  if(req.get("x-github-event") !== "push") {
+    next();
+    return;
+  }
+
+  debug("Master pushed");
+  res.json({});
+}
+
 /* The github webhook endpoint */
 router.post('/webhook', function(req, res, next) {
   debug("Headers: " + JSON.stringify(req.headers, null, 2));
   debug("Body: " + JSON.stringify(req.body, null, 2));
-  res.json({});
-});
+  next();
+},
+masterPush);
 
 module.exports = router;
