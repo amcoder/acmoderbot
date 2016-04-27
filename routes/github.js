6895ba6762cc27bd0ccf8c7f657b0d905b32b9b8
Andy Miller <amcoder@gmail.com>
2016-04-27 15:23:33 -0400

    +1 when someone comments on my issue

--- routes/github.js
+++ routes/github.js
@@ -1,6 +1,39 @@
 var express = require('express');
 var debug = require('debug')('amcoderbot:github');
 var router = express.Router();
+var http = require('https');
+var url = require('url');
+
+var plusOne = function(uri) {
+  debug("Commenting on " + uri);
+
+  var token = process.env.GITHUB_TOKEN;
+
+  var options = url.parse(uri);
+  options.method = "POST";
+  options.headers = {
+    "Content-Type": "application/json",
+    "Authorization": "token " + token,
+    "user-agent": "amcoderbot"
+  }
+
+  debug(options);
+
+  var req = http.request(options, (res) => {
+    debug("Status: " + res.statusCode);
+    debug(res.headers);
+  });
+
+  req.on('error', (e) => {
+    debug('problem with request: ${e.message}');
+  });
+
+  // write data to request body
+  req.write(JSON.stringify({ body: ":+1:" }));
+  req.end();
+
+  debug("Commented");
+}
 
 var logRequest = function(req, res, next) {
   debug("Headers: " + JSON.stringify(req.headers, null, 2));
@@ -13,6 +46,8 @@ var issueComment = function(req, res, next) {
       req.body.issue.user.login === "amcoderbot" &&
       req.body.comment.user.login !== "amcoderbot") {
     debug("Issue comment");
+
+    plusOne(req.body.issue.comments_url);
   }
   next();
 }
